eab24d01b59e50d4319de60fd12254e0fad256fc
Junnie-Jobs <dj0999@naver.com>
2016-06-03 17:19:03 +0900

    pom.xml update

--- src/main/java/blog/config/ServletInitializer.java
+++ src/main/java/blog/config/ServletInitializer.java
@@ -1,7 +1,9 @@
-package core;
+package blog.config;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+import core.JunnieJobsBlogApplication;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
